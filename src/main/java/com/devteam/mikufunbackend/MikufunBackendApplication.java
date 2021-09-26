8cf0bd506223a69e3ce09003344c755286e4415f
Jackisome <happydyj603@126.com>
2021-09-26 14:11:53 +0800

    初始化

---
+++ src/main/java/com/devteam/mikufunbackend/MikufunBackendApplication.java
@@ -0,0 +1,13 @@
+package com.devteam.mikufunbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MikufunBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MikufunBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/devteam/mikufunbackend/MikufunBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devteam.mikufunbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MikufunBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

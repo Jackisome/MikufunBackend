abf1c9f84a571b458a729c085820a67e1bd02c95
Jackisome <happydyj603@126.com>
2021-09-26 23:34:58 +0800

    temp

--- src/main/java/com/devteam/mikufunbackend/controller/TestController.java
+++ src/main/java/com/devteam/mikufunbackend/controller/TestController.java
@@ -2,8 +2,6 @@ package com.devteam.mikufunbackend.controller;
 
 import com.devteam.mikufunbackend.util.Response;
 import com.devteam.mikufunbackend.util.ResultUtil;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 

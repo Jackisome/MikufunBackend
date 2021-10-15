6ff771f02d9364f4470be010f4936a215597604c
mouette <seagulli@qq.com>
2021-10-15 23:56:36 +0800

    修改搜索接口

--- src/main/java/com/devteam/mikufunbackend/service/serviceInterface/FavoriteService.java
+++ src/main/java/com/devteam/mikufunbackend/service/serviceInterface/FavoriteService.java
@@ -1,6 +1,9 @@
 package com.devteam.mikufunbackend.service.serviceInterface;
 
 
+import java.util.List;
+import java.util.Map;
+
 public interface FavoriteService {
 
     /**
@@ -18,5 +21,13 @@ public interface FavoriteService {
      * @return 收藏状态
      */
     String getFavoriteStatus(Integer resourceId);
+
+    /**
+     * 获取指定番剧列表的收藏状态
+     *
+     * @param resourceIds 番剧 Id 列表
+     * @return 番剧 Id 及其收藏状态映射
+     */
+    Map<Integer, String> getFavoriteStatusListById(List<Integer> resourceIds);
 
 }
